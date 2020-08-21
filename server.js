fd7129e085b98c2f16b34abed8cafb93d2de9a23
luanpa <69101674+luanpa@users.noreply.github.com>
2020-08-21 18:50:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require("express");
+//novo
+const exphbs = require('express-handlebars');
+const path = require('path');
+const Sequelize = require('sequelize');
+const OP = Sequelize.Op;
+////
 const app = express();
 const db = require("./models");
 const PORT = process.env.PORT || 3000;
+
+//static folder novo
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+//hadle brs novo
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 
 ///body
 const bodyParser = require("body-parser");
@@ -14,7 +31,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-const apiRoutes = require('./routes/apiRoutes')
+const apiRoutes = require('./routes/apiRoutes');
 app.use('/api', apiRoutes);
 
 
@@ -22,17 +39,38 @@ db.sequelize.sync().then(()=>{
     app.listen(PORT, () =>{
         console.log(`listen on: http://localhost:${PORT}`);
     });
-}); 
+});  
 
 app.get('/',(req, res) => {
+    
+    let search = req.query.job;
+    let query = '%'+search+'%'; //php => php macanismo de busca
 
-    db.todoE.findAll({order: [
+    if(!search) {
+         db.todoE.findAll({order: [
         ['createdAt','DESC']
     ]})
     .then(todoE => {
        res.render ('index',{
             todoE
         });
-    });
+    })
+    .catch(err => console.log(err));
+    } else {
+        db.todoE.findAll({
+            where: {title: {[OP.like]: query}},
+            order: [
+            ['createdAt','DESC']
+        ]})
+        .then(todoE => {
+           res.render ('index',{
+                todoE, search
+            });
+        })
+        .catch(err => console.log(err));
+    }
+
+   
 
 });
+ 
